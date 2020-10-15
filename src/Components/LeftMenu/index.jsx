52cdad563345215ae7f147388a5495b8c2dc74a6
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-10-15 22:31:51 +0200

    add tasks link and icon to menu

--- src/Components/LeftMenu/index.jsx
+++ src/Components/LeftMenu/index.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom'
 export const LeftMenuComponent = () => {
     return (
         <Wrapper>
+
             <Link to="/"><Icon className="fa fa-home"></Icon></Link>
+            <Link to="/tasks">  <Icon className="fa fa-tasks" ></Icon></Link>
             <Link to="/charts"> <Icon className="fa fa-bar-chart-o"></Icon></Link>
             <Link to="/clients"><Icon className="fa fa-users"></Icon></Link>
             <Link to="/calendar"><Icon className="fa fa-calendar"></Icon></Link>
-
 
 
         </Wrapper >
