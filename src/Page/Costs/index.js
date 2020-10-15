f07415a19277e005c3d9ff182dcbb164bde711c4
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-10-15 22:42:05 +0200

    create costs page

---
+++ src/Page/Costs/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const CostsPage = () => {
+    return (
+        <div>
+            <TopMenuComponent />
+
+            <Content>
+                <LeftMenuComponent />
+                <RightSide>
+                    Costs
+                </RightSide>
+            </Content>
+        </div >
+    )
+}
