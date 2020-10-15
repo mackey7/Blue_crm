24dc9f569f1aa9caad907a44817feb340cf3fe8e
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-10-15 21:57:54 +0200

    add link to home in menu

--- src/Components/LeftMenu/index.jsx
+++ src/Components/LeftMenu/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Wrapper, Icon } from '../../Helpers/Styles/LeftMenu/index'
+import { Link } from 'react-router-dom'
 export const LeftMenuComponent = () => {
     return (
         <Wrapper>
-            <Icon className="fa fa-home"></Icon>
+            <Link to="/">  <Icon className="fa fa-home"></Icon></Link>
+
+
             <Icon className="fa fa-bar-chart-o"></Icon>
             <Icon className="fa fa-users"></Icon>
             <Icon className="fa fa-calendar"></Icon>
