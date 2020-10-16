635d78c6b91aa597b790f8951314f61aa1a7761a
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-10-16 21:07:03 +0200

    create tasks list component

---
+++ src/Components/TasksComponent/TasksList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export const TasksList = () => {
+    return (
+        <div>
+            TasksList
+        </div>
+    )
+}
--- src/Components/TasksComponent/index.js
+++ src/Components/TasksComponent/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { AddTask } from './addTask'
-
+import { TasksList } from './TasksList'
 
 export const TasksComponent = () => {
 
     return (
         <div>
             <AddTask />
+            < TasksList />
         </div>
     )
 }
