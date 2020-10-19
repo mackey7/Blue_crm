f08804b1ad85e7c636daac1219b892be5db25185
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-10-19 22:06:48 +0200

    create Clients Component and add to Clients page

---
+++ src/Components/ClientsComponent/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ClientsComponent = () => {
+    return (
+        <div>
+            ClientsComponent
+        </div>
+    )
+}
--- src/Page/Clients/index.js
+++ src/Page/Clients/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { TopMenuComponent } from '../../Components/TopMenuComponent/index'
 import { LeftMenuComponent } from '../../Components/LeftMenu/index'
 import { Content, RightSide } from '../../Helpers/Styles/HomePage/index'
-
+import { ClientsComponent } from '../../Components/ClientsComponent/'
 
 export const ClientsPage = () => {
 
@@ -14,7 +14,7 @@ export const ClientsPage = () => {
             <Content>
                 <LeftMenuComponent />
                 <RightSide>
-                    Clients
+                    <ClientsComponent />
                 </RightSide>
             </Content>
         </div >
