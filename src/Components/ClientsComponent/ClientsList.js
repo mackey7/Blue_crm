44ef8e75ea543f7f0a6f23e3054338f822fa6bef
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-10-20 20:05:09 +0200

    create Clients List Component

---
+++ src/Components/ClientsComponent/ClientsList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ClientsList = () => {
+    return (
+        <div>
+            ClientsList
+        </div>
+    )
+}
--- src/Components/ClientsComponent/index.js
+++ src/Components/ClientsComponent/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { AddClient } from './AddClient'
+import { ClientsList } from './ClientsList'
 export const ClientsComponent = () => {
     return (
         <div>
             < AddClient />
+            <ClientsList />
         </div>
     )
 }
