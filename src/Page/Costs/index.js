d35d6b69e72d0ef6fd1d7597bacd3d08d073baa8
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-10-21 20:29:29 +0200

    create cost component to page

--- src/Page/Costs/index.js
+++ src/Page/Costs/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { TopMenuComponent } from '../../Components/TopMenuComponent/index'
 import { LeftMenuComponent } from '../../Components/LeftMenu/index'
 import { Content, RightSide } from '../../Helpers/Styles/HomePage/index'
-
+import { CostsComponent } from '../../Components/CostsComponent/index'
 export const CostsPage = () => {
     return (
         <div>
@@ -11,7 +11,7 @@ export const CostsPage = () => {
             <Content>
                 <LeftMenuComponent />
                 <RightSide>
-                    Costs
+                    <CostsComponent />
                 </RightSide>
             </Content>
         </div >
