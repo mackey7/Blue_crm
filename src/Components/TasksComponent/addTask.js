686c217065cc71b2382a5b39e37826bfdedc2368
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-11-06 20:18:53 +0100

    connect addtask component with firebase

--- src/Components/TasksComponent/addTask.js
+++ src/Components/TasksComponent/addTask.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ContainerWrapper, Form, AddBtn } from '../../Helpers/Styles/TasksComponent/addTask'
+import app from '../../base'
 export const AddTask = () => {
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
+    function onSubmit(e) {
+        e.preventDefault()
+        app
+            .firestore()
+            .collection('tasks')
+            .add({
+                title,
+                description,
+                isDone: false
+            })
+            .then(() => {
+                setTitle('')
+                setDescription('')
+            })
+    }
+
     return (
         <ContainerWrapper>
-            <Form >
+            <Form onSubmit={onSubmit}>
                 <label htmlFor="Title">Title:</label>
-                <input type="text" id="Title" name="Title" />
+                <input type="text" id="Title" name="Title" value={title} onChange={e => setTitle(e.currentTarget.value)} />
                 <label htmlFor="Description">Description</label>
-                <textarea id="Description" name="Description" rows="4" cols="50" />
+                <textarea id="Description" name="Description" rows="4" cols="50" value={description} onChange={e => setDescription(e.currentTarget.value)} />
                 <AddBtn type="submit" value="ADD" />
             </Form>
         </ContainerWrapper>
