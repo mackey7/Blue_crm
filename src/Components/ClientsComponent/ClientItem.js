3f4e40e1967d93395dedae72117550d1514315cf
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-11-06 23:04:06 +0100

    add function to delete item from firebase

--- src/Components/ClientsComponent/ClientItem.js
+++ src/Components/ClientsComponent/ClientItem.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { DeleteBtn } from '../../Helpers/Styles/ClientsComponent/ClientItem'
+import app from '../../base'
+
 export const ClientItem = clientData => {
-
-
-
-
+    function deleteItem(e) {
+        app.firestore()
+            .collection('clients')
+            .doc(e.target.getAttribute("data-id"))
+            .delete()
+    }
     return (
 
         <tr>
             <td>{clientData.clientData.name}</td> <td>{clientData.clientData.surname}</td>  <td>{clientData.clientData.email}</td>   <td>{clientData.clientData.street}</td>    <td>{clientData.clientData.zipCode}</td>  <td>{clientData.clientData.city}</td>  <td>
-                <DeleteBtn className="fa fa-trash" aria-hidden="true"></DeleteBtn>
+                <DeleteBtn onClick={e => deleteItem(e)} data-id={clientData.clientData.id} className="fa fa-trash" aria-hidden="true"></DeleteBtn>
             </td>
         </tr>
 
