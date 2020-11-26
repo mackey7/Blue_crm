cb58bf9517aed2359a8cf14ced6bf8c7ca35ab41
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-11-26 21:43:30 +0100

    create connections costs reducer with root

--- src/Reducers/rootReducer.js
+++ src/Reducers/rootReducer.js
@@ -3,12 +3,14 @@ import { firebaseReducer } from 'react-redux-firebase'
 import { firestoreReducer } from 'redux-firestore'
 import { TasksReducer } from './TasksReducers'
 import { ClientsReducer } from './ClientsReducers'
+import { CostsReducer } from './CostsReducers'
 
 const rootReducer = combineReducers({
     firebase: firebaseReducer,
     firestore: firestoreReducer,
     tasks: TasksReducer,
-    clients: ClientsReducer
+    clients: ClientsReducer,
+    costs: CostsReducer
 
 })
 
