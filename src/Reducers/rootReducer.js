2d201d50f684d9e60738023a9d36e7b81962e90f
Maciej Bogdanski <m.bogdanskii@gmail.com>
2020-11-26 19:05:11 +0100

    connect ClientsReducer with root

--- src/Reducers/rootReducer.js
+++ src/Reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import { firebaseReducer } from 'react-redux-firebase'
 import { firestoreReducer } from 'redux-firestore'
 import { TasksReducer } from './TasksReducers'
+import { ClientsReducer } from './ClientsReducer'
 
 const rootReducer = combineReducers({
     firebase: firebaseReducer,
     firestore: firestoreReducer,
-    tasks: TasksReducer
+    tasks: TasksReducer,
+    clients: ClientsReducer
 
 })
 
